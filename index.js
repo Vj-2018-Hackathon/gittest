c57fda94ec73640f3760545006ed904a21b7e0e5
Vijaya <malladi.vijayalaxmi@gmail.com>
2017-12-30 17:11:11 -0600

    callbacks and err handlings

---
+++ index.js
@@ -0,0 +1,27 @@
+﻿var rect = require('./rectangle');
+
+
+
+function solverectange(l, b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+            console.log("ERROR: ", err.message);
+        }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
+
+
+solverectange(2,3);
+solverectange(10,12);
+solverectange(0,3);
+solverectange(-3,0);
+
---
+++ rectangle.js
@@ -0,0 +1,27 @@
+﻿//var rect = {
+//    perimeter: (x, y) => (2 * (x + y)),
+//    area : (x,y) => (x*y)
+//}
+//function solveReact(l, b) {
+//    console.log(rect.area(l, b));
+//    console.log(rect.perimeter(l, b));
+
+//}
+
+//solveReact(2, 3);
+//solveReact(0.6);
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0)
+        setTimeout(() =>
+            callback(new Error("Rectangle dimensions should be greater than zero: l = "
+                + x + ", and b = " + y),
+            null),
+            2000);
+    else
+        setTimeout(() =>
+            callback(null, {
+                perimeter: () => (2 * (x + y)),
+                area: () => (x * y)
+            }),
+            2000);
+}
